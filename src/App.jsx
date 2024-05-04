68e24d3763b82222bb411df3b3c5b063315efbac
Royal Finch <Royal2791@gmail.com>
2024-05-04 14:18:14 -0400

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SinglePrice from './SinglePrice';
 export default function App() {
   return (
     <main>
+      <h1 className="sr-only">single price grid component</h1>
       <SinglePrice />
     </main>
   );
--- src/SinglePrice.jsx
+++ src/SinglePrice.jsx
@@ -3,8 +3,8 @@ export default function SinglePrice() {
     <div className="single-price">
       <div className="single-price__top-half">
         <h2>Join our community</h2>
-        <h3>30-day, hassle-free money back guarantee</h3>
-        <p>
+        <h3 className="yellow-txt">30-day, hassle-free money back guarantee</h3>
+        <p className="top-half__p">
           Gain access to our full library of tutorials along with expert code
           reviews. Perfect for any developers who are serious about honing their
           skills.
@@ -12,19 +12,25 @@ export default function SinglePrice() {
       </div>
       <div className="single-price__subscription">
         <h3>Monthly Subscription</h3>
-        <span>
-          <span>$29</span> per month
+        <span className="price-per">
+          <span className="price-text">$29</span> per month
         </span>
         <p>Full access for less than $1 a day</p>
-        <a href="">Sign Up</a>
+        <a className="btn-sign-up" href="">
+          Sign Up
+        </a>
       </div>
       <div className="single-price__why-us">
         <h3>Why Us</h3>
-        <p>
-          Tutorials by industry experts Peer & expert code review Coding
-          exercises Access to our GitHub repos Community forum Flashcard decks
-          New videos every week
-        </p>
+        <ul className="why-us__ul">
+          <li>Tutorials by industry experts</li>
+          <li>Peer & expert code review</li>
+          <li>Coding exercises</li>
+          <li>Access to our GitHub repos</li>
+          <li>Community forum</li>
+          <li>Flashcard decks</li>
+          <li>New videos every week</li>
+        </ul>
       </div>
     </div>
   );
